97c892565deb4d874caf96db9d49ff99d8ba301e
EjimEgwu <ejimkeonyeegwu10@gmail.com>
2026-08-15 02:35:04 +0100

    Trim outputs.tf to exactly 5 root outputs per Section 12 spec

--- terraform/envs/outputs.tf
+++ terraform/envs/outputs.tf
@@ -3,17 +3,3 @@ output "cluster_name" { value = module.eks.cluster_name }
 output "region" { value = "us-east-1" }
 output "vpc_id" { value = module.vpc.vpc_id }
 output "assets_bucket_name" { value = module.s3_lambda.bucket_name }
-output "alb_controller_role_arn" { value = module.alb_controller.alb_controller_role_arn }
-output "dev_view_access_key_id" {
-  value = module.iam_dev_user.dev_view_access_key_id
-}
-
-output "dev_view_secret_access_key" {
-  value     = module.iam_dev_user.dev_view_secret_access_key
-  sensitive = true
-}
-
-output "dev_view_console_password" {
-  value     = module.iam_dev_user.dev_view_console_password
-  sensitive = true
-}
